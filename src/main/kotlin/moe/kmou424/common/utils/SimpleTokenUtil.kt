db9ef76f361f4904f020a87a7b655bc2f1e902c9
kmou424 <me@kmou424.moe>
2022-11-06 12:33:10 +0800

    chore(token): Don't use delimiter as token character

--- src/main/kotlin/moe/kmou424/common/utils/SimpleTokenUtil.kt
+++ src/main/kotlin/moe/kmou424/common/utils/SimpleTokenUtil.kt
@@ -4,7 +4,7 @@ object SimpleTokenUtil {
     private const val TokenLen = 32;
 
     fun generate(tokenLen: Int = TokenLen): String {
-        val str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.;+=-_()"
+        val str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-_()"
         var token = ""
         for (i in 0 until tokenLen) {
             token += str[(1..str.length).random() - 1]
